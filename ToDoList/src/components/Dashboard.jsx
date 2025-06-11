83b63d2000b6ebd22fc8b345bf471bd23eb5901a
Ayslan Francisco Monteiro Neto <ayslanfrancisco999@gmail.com>
2025-06-11 08:26:46 -0300

    feat: Implementando temas no projeto ToDoList

--- ToDoList/src/components/Dashboard.jsx
+++ ToDoList/src/components/Dashboard.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react"
 import "./Dashboard.css"
 
 export default function Dashboard({children}){
+
+    const [tema, setTema] = useState("ligth")
+    function mudaTema(){
+        if (tema === "ligth"){
+            setTema("dark")
+        }else{
+            setTema("ligth")
+        }
+    }
+
     return (
-        <div className="dashboard">
+        <div className={`dashboard ${tema}`}>
             <h1>Dashboard Tarefas</h1>
-            
+            <button onClick={mudaTema}>Mudar tema</button>
             {children}
         </div>
     )
