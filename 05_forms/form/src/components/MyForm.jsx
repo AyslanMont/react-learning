158ba7a2d1bf152ffea161f62612d9b13ee928f3
Ayslan Francisco Monteiro Neto <ayslanfrancisco999@gmail.com>
2025-06-27 07:37:53 -0300

    Evento onChange

--- 05_forms/form/src/components/MyForm.jsx
+++ 05_forms/form/src/components/MyForm.jsx
@@ -1,15 +1,24 @@
-import "./MyForm.css    "
+import "./MyForm.css"
+import {useState} from 'react'
 
 export default function MyForm(){
+    // 3 - Gerenciamento de dados
+    const [name, setName] = useState()
+    const [email, setEmail] = useState()
+    
+    const handlename = (e) => {
+        setName(e.target.value)
+    }
+
     return (
         <div>
-            {/* Criação de form */}
+            {/* 1 - Criação de form */}
             <form>
                 <div>
                     <label htmlFor="name">Nome:</label>
-                    <input type="text" name="name" placeholder="Digite seu nome..."/>
+                    <input type="text" name="name" placeholder="Digite seu nome..." onChange={handlename}/>
                 </div>
-                {/* Label envolvendo input */}
+                {/* 2 - Label envolvendo input */}
                 <label>
                     <span>E-mail:</span>
                     <input type="email" name="email" placeholder="Digite seu email..."/>
